4b568deb008959306167943d5a3d6ad534acf80d
OlehKozubJS <oleg.kozub54@gmail.com>
2023-08-30 20:08:48 +0300

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,23 +1,23 @@
-import { ContactForm } from './ContactForm';
-import { ContactList } from './ContactList';
-import { Filter } from './Filter';
-import { LoadAlert } from './LoadAlert';
-import PhonebookStyles from './PhonebookCSS/Pnonebook.module.css';
-import { useSelector } from 'react-redux';
-import { selectIsLoading, selectError } from '../redux/selectors';
+import { lazy, Suspense } from 'react';
+import { Routes, Route } from 'react-router-dom';
+import AppCSS from './css/App.module.css';
+import Navigation from 'components/Navigation';
+
+const lazyImport = newComponent => {
+  return lazy(() => import(`../pages/${newComponent}`));
+};
 
 export const App = () => {
   const isLoading = useSelector(selectIsLoading);
   const error = useSelector(selectError);
 
   return (
-    <div className={PhonebookStyles.phonebook}>
-      <h1 className={PhonebookStyles.phonebookHeader}>Phonebook</h1>
-      <ContactForm />
-      {isLoading && !error && <LoadAlert>Request in progress...</LoadAlert>}
-      <h2 className={PhonebookStyles.contactsHeader}>Contacts</h2>
-      <Filter className="filterInput" />
-      <ContactList className="contactList" />
-    </div>
+    <Suspense>
+      <Routes>
+        <Route />
+        <Route />
+        <Route />
+      </Routes>
+    </Suspense>
   );
 };
