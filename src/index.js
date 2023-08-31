d51c9eca4d1ea5ffe6e586173951a182e0f2a79f
OlehKozubJS <oleg.kozub54@gmail.com>
2023-08-31 19:30:44 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ import { store } from './redux/store';
 import { BrowserRouter } from 'react-router-dom';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <BrowserRouter basename="">
+  <BrowserRouter basename="/goit-react-hw-08-phonebook">
     <Provider store={store}>
       <App />
     </Provider>
