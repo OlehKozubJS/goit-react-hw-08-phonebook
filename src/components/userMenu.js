89a2a17458105b8fd323fe36f06d1546becb08c9
OlehKozubJS <oleg.kozub54@gmail.com>
2023-09-03 12:38:25 +0300

    Update userMenu.js

--- src/components/userMenu.js
+++ src/components/userMenu.js
@@ -1,3 +1,18 @@
 import { useDispatch } from 'react-redux';
 import { logOut } from 'redux/auth/operations';
-//import {useAuth}
+import { useAuth } from 'hooks';
+//export css from './';
+
+export const UserMenu = () => {
+  const dispatch = useDispatch();
+  const { user } = useAuth();
+
+  return (
+    <div className="">
+      <p>Welcome, {userName}</p>
+      <button type="button" onClick={() => dispatch(logOut())}>
+        Logout
+      </button>
+    </div>
+  );
+};
