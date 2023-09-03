da2cce76f079feff560382b5967f79adc5f54ee5
OlehKozubJS <oleg.kozub54@gmail.com>
2023-09-03 15:58:13 +0300

    Update Login.jsx

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,3 +1,10 @@
+import { LoginForm } from 'components/LoginForm';
+
 export default function Home() {
-  return <div></div>;
+  return (
+    <div>
+      <h2>Login</h2>
+      <LoginForm />
+    </div>
+  );
 }
