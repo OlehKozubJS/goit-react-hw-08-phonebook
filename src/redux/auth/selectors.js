76b54c770d3a3b9d902c0e61c900bef58a7eb46c
OlehKozubJS <oleg.kozub54@gmail.com>
2023-09-03 18:32:43 +0300

    Update selectors.js

--- src/redux/auth/selectors.js
+++ src/redux/auth/selectors.js
@@ -1,4 +1,3 @@
-export const selectContacts = state => state.contacts.items;
-export const selectIsLoading = state => state.contacts.isLoading;
-export const selectError = state => state.contacts.error;
-export const selectFilter = state => state.filter;
+export const selectIsLoggedIn = state => state.auth.isLoggedIn;
+export const selectUser = state => state.auth.user;
+export const selectIsRefreshing = state => state.auth.isRefreshing;
