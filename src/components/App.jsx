04ed1bcb79906c706b81aadc56ed8c3460eb2ed3
OlehKozubJS <oleg.kozub54@gmail.com>
2023-09-03 22:45:06 +0300

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -33,7 +33,12 @@ export const App = () => {
           <Route index element={<HomePage />} />
           <Route path="/register" element={<RegisterPage />} />
           <Route path="/login" element={<LoginPage />} />
-          <Route path="/contacts" element={<ContactsPage />} />
+          <Route
+            path="/contacts"
+            element={
+              <PrivateRoute redirectTo="/login" component={<ContactsPage />} />
+            }
+          />
         </Route>
       </Routes>
     </Suspense>
