586bc900a315f2974aaf34ab41aa29c389482b01
OlehKozubJS <oleg.kozub54@gmail.com>
2023-09-03 18:06:31 +0300

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -16,7 +16,7 @@ export const App = () => {
   return (
     <Suspense>
       <Routes>
-        <Route path="/" element={HomePage} />>
+        <Route path="/" element={HomePage} />
         <Route path="/register" element={RegisterPage} />
         <Route path="/login" element={LoginPage} />
         <Route path="/contacts" element={ContactsPage} />
