a1ae5333f6a89ee5635ddadda8b203024d84c1ae
OlehKozubJS <oleg.kozub54@gmail.com>
2023-09-03 14:27:55 +0300

    Update Navigation.jsx

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -1,5 +1,7 @@
 import { NavLink } from 'react-router-dom';
 import { useAuth } from 'hooks';
+import { AuthNav } from './AuthNav';
+import { UserMenu } from './userMenu';
 //import css from './PhonebookCSS/Navigation.module.css';
 
 export const Navigation = () => {
@@ -7,8 +9,11 @@ export const Navigation = () => {
 
   return (
     <nav>
-      <NavLink to="/">Home</NavLink>
-      {isLoggedIn && <NavLink to="tasks">Tasks</NavLink>}
+      <div>
+        <NavLink to="/">Home</NavLink>
+        {isLoggedIn && <NavLink to="tasks">Tasks</NavLink>}
+      </div>
+      <div></div>
     </nav>
   );
 };
