a73053e894f2d4ffb04ab73d3e1fc6c3501a91cf
OlehKozubJS <oleg.kozub54@gmail.com>
2023-09-03 15:29:53 +0300

    Update RegisterForm.jsx

--- src/components/RegisterForm.jsx
+++ src/components/RegisterForm.jsx
@@ -4,4 +4,17 @@ import { register } from 'redux/auth/operations';
 
 export const RegisterForm = () => {
   const dispatch = useDispatch();
+
+  const handleSubmit = event => {
+    event.preventDefault();
+    const form = event.currentTarget;
+    dispatch(
+        register({
+            name: form.elements.name.value,
+            email: form.elements.email.value,
+            password: form.elements.password.value,s
+        });
+    );
+    form.reset();
+  };
 };
