3ae6645087962cda27988379628abd06c8db1ff2
OlehKozubJS <oleg.kozub54@gmail.com>
2023-09-03 18:06:00 +0300

    Update Layout.js

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,3 +1,14 @@
 import { Outlet } from 'react-router-dom';
 import { Navigation } from './Navigation';
 import { Suspense } from 'react';
+
+export const Layout = () => {
+  return (
+    <div>
+      <Navigation />
+      <Suspense fallback={null}>
+        <Outlet />
+      </Suspense>
+    </div>
+  );
+};
