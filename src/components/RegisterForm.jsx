520971744048a55db94bf79dd135f507baf717a0
OlehKozubJS <oleg.kozub54@gmail.com>
2023-09-03 15:16:40 +0300

    Create RegisterForm.jsx

---
+++ src/components/RegisterForm.jsx
@@ -0,0 +1,7 @@
+import { useDispatch } from 'react-redux';
+import { register } from 'redux/auth/operations';
+//import css
+
+export const RegisterForm = () => {
+  const dispatch = useDispatch();
+};
