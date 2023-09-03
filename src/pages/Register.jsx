e62b0b3a890848593745a897db9c77887497b0d0
OlehKozubJS <oleg.kozub54@gmail.com>
2023-09-03 16:01:13 +0300

    Update Register.jsx

---
+++ src/pages/Register.jsx
@@ -0,0 +1,10 @@
+import { RegisterForm } from 'components/RegisterForm';
+
+const Register = () => {
+  return (
+    <div>
+      <h2>Registration</h2>
+      <RegisterForm />
+    </div>
+  );
+};
