dfbe487bd4aebdfed26f202b06a19c943bad0e76
OlehKozubJS <oleg.kozub54@gmail.com>
2023-09-04 11:55:19 +0300

    update nav styles

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -2,8 +2,8 @@ import { NavLink } from 'react-router-dom';
 import { useAuth } from 'hooks';
 import { AuthNav } from './AuthNav';
 import { UserMenu } from './UserMenu';
-//import css from '../components/PhonebookCSS/Navigation.module.css';
-import css from '../components/PhonebookCSS/Pnonebook.module.css';
+import css from '../components/PhonebookCSS/Navigation.module.css';
+//import css from '../components/PhonebookCSS/Pnonebook.module.css';
 
 export const Navigation = () => {
   const { isLoggedIn } = useAuth();
