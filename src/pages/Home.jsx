e0338c0fe27724e228f866c2333a5329793b2f94
OlehKozubJS <oleg.kozub54@gmail.com>
2023-09-05 11:28:48 +0300

    Update Home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,7 +3,7 @@ import css from '../components/PhonebookCSS/Pnonebook.module.css';
 const Home = () => {
   return (
     <div className={css.homeHeader}>
-      <h1>Welcome</h1>
+      <h1>Welcome!</h1>
     </div>
   );
 };
