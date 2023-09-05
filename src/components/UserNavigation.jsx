36bf3e0ffb5a21e3d79220d8e2adb937643dd47f
OlehKozubJS <oleg.kozub54@gmail.com>
2023-09-05 22:23:32 +0300

    Update UserNavigation.jsx

--- src/components/UserNavigation.jsx
+++ src/components/UserNavigation.jsx
@@ -1,4 +1,3 @@
-import { NavLink } from 'react-router-dom';
 import { useAuth } from 'hooks';
 import css from '../components/PhonebookCSS/Navigation.module.css';
 import { useDispatch } from 'react-redux';
@@ -6,43 +5,18 @@ import { logOut } from 'redux/auth/operations';
 
 export const UserNavigation = () => {
   const dispatch = useDispatch();
-  const { isLoggedIn, user } = useAuth();
+  const { user } = useAuth();
 
   return (
-    <nav className={css.navigation}>
-      <div className={css.navSection}>
-        <NavLink className={css.styledLink} to="/">
-          Home
-        </NavLink>
-        {isLoggedIn && (
-          <NavLink className={css.styledLink} to="/contacts">
-            Contacts
-          </NavLink>
-        )}
-      </div>
-      <div>
-        {isLoggedIn ? (
-          <div className={css.navSection}>
-            <p className={css.welcomeText}>Welcome, {user.name}!</p>
-            <button
-              className={css.styledLink}
-              type="button"
-              onClick={() => dispatch(logOut())}
-            >
-              Logout
-            </button>
-          </div>
-        ) : (
-          <div className={css.navSection}>
-            <NavLink className={css.styledLink} to="/register">
-              Register
-            </NavLink>
-            <NavLink className={css.styledLink} to="/login">
-              Log In
-            </NavLink>
-          </div>
-        )}
-      </div>
-    </nav>
+    <div className={css.navSection}>
+      <p className={css.welcomeText}>Welcome, {user.name}!</p>
+      <button
+        className={css.styledLink}
+        type="button"
+        onClick={() => dispatch(logOut())}
+      >
+        Logout
+      </button>
+    </div>
   );
 };
