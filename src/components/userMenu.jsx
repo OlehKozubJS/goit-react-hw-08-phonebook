3d8682da6d742dbe5f54d595bc7e20de6e9f559a
OlehKozubJS <oleg.kozub54@gmail.com>
2023-09-05 11:45:28 +0300

    Update userMenu.jsx

--- src/components/userMenu.jsx
+++ src/components/userMenu.jsx
@@ -1,7 +1,7 @@
 import { useDispatch } from 'react-redux';
 import { logOut } from 'redux/auth/operations';
 import { useAuth } from 'hooks';
-import css from '../components/PhonebookCSS/Navigation.module.css';
+//import css from '../components/PhonebookCSS/Navigation.module.css';
 import navCss from '../components/PhonebookCSS/Navigation.module.css';
 
 export const UserNav = () => {
